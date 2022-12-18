13f49a5b4ce22fe2e61858c8ffd9a92429758d87
Teyko <lepennecguilian@gmail.com>
2022-12-18 17:42:06 +0100

    Release version. Everything is commented, somewhat optimised, every planned function is included, images have alts, the cog has been modified to stand upright and simply spin, and there are two commented out code to: in css remove the blinking gear at page load, in js have the cubes pop and depop smoothly if we want to give them a life span.

--- Assets/Javascript/drawBackground.js
+++ Assets/Javascript/drawBackground.js
@@ -1,73 +1,97 @@
-// ------------------------------------------------ Scenes rendering ------------------------------------------------
 function drawBackground(canvas, deltaTime, timeSinceStart){
     
-    // Optional block (if removed, increase base block number)
+    // During the intro, generate a new object every ~= 0.3 seconds so as to feel more full.
     if (timeSinceStart%0.3 < timeSinceStart*0.1 && timeSinceStart < animationDelay){
+        // Generate a random translation vector to make the shape appear anywhere on the screen.
         let translateVector = [randInterval(-35., 35.), randInterval(-25., 25.), randInterval(-40., -22.)];
+        // Generate a random rotation vector so as for all objects to spin differently.
         let rotationVector = [randInterval(0.1, 0.5), randInterval(0.1, 0.5), randInterval(0.1, 0.5)];
-        canvas.objectTimeAlive.push(0.0);
-        canvas.objectLifeSpan.push(randInterval(30.0, 100.0));
-        canvas.transVList.push(translateVector);
-        canvas.depthList.push(translateVector[2]);
-        canvas.rotVList.push(rotationVector);
-    } else {
-        // Refurbish the list of objects if needed.
-        while (canvas.transVList.length < canvas.objectCount){
-            let translateVector = [gapRandInterval(-25., -10, 10, 25.), randInterval(-15., 15.), randInterval(-40., -22.)];
-            let rotationVector = [randInterval(0.1, 0.5), randInterval(0.1, 0.5), randInterval(0.1, 0.5)];
-            canvas.objectTimeAlive.push(0.0);
-            canvas.objectLifeSpan.push(randInterval(10000.0, 10000.0));
-            canvas.transVList.push(translateVector);
-            canvas.depthList.push(translateVector[2]);
-            canvas.rotVList.push(rotationVector);
-        };
-    }
+        // Generate a random life expectancy for the cube, it will despawn if this end of life is reached.
+        let objectLifeSpan = randInterval(30.0, 100.0);
+        // Push the data to the linked canvas object.
+        canvas.pushObjectData(translateVector, rotationVector, objectLifeSpan);
+    } 
 
+    // Refurbish the list of objects if it is below the desired object count.
+    while (canvas.transVList.length < canvas.objectCount){
+        let translateVector = [gapRandInterval(-25., -10, 10, 25.), randInterval(-15., 15.), randInterval(-40., -22.)];
+        // Generate a random rotation vector so as for all objects to spin differently.
+        let rotationVector = [randInterval(0.1, 0.5), randInterval(0.1, 0.5), randInterval(0.1, 0.5)];
+        // once the animation is done, I decided that cubes shouldn't despawn, if this is to change, simply lower this values and uncomment the code down below.
+        let objectLifeSpan = randInterval(10000.0, 10000.0);
+        // Push the data to the linked canvas object.
+        canvas.pushObjectData(translateVector, rotationVector, objectLifeSpan);
+    };
 
+    // If the current time is situated between the planned end of the intro to a maximum of it +0.03s (so as to be sure to capture the moment at intro end in this statement):
     if(timeSinceStart > animationDelay && timeSinceStart < (animationDelay + 0.03)){
+        // Signal that this is the last run of the intro animations.
         canvas.animationEnd = true;
     }
     
+    // Iterate over the objects by index.
     for (let i = 0; i < canvas.transVList.length; i++){
-        let sumPosition = 0;
+        // Compute how close the object is to the center of the screen by adding the sum of it's absolute x and y coordinates, if the object is in the center then we should
+        // Delete it so as to generate another one that we can see, it keep the intro going more dynamically and ensure the smooth despawning of still cubes if enabled.
+        let sumPosition = canvas.transVList[i].slice(0, 2).reduce((a, b) => Math.abs(a) + Math.abs(b));
+        // Use a switch case to tidy different cases, modifications to objects places are made by modifying their transVList array.
         switch(true){
+            // If the intro is currently going and the index is the first one, meaning the main, big growing block:
             case (timeSinceStart < animationDelay && i === 0):
+                // Then ensure that it won't be deleted based on it's closeness to the center by setting it's sumPosition to something bigger than the threshold.
                 sumPosition = 2;
-                canvas.transVList[i] = [0, 0, Math.min((-20+(((timeSinceStart+3)**4)*0.01)), 0)];
+                // and move it toward the screen to a max of -2. For this we use a function of time that adds more and more to the base of -20.
+                canvas.transVList[i] = [0, 0, Math.min((-20+(((timeSinceStart+3)**4)*0.01)), -2)];
                 break;
+            // If the intro is still going but this time for every object that is not the first (growing) one:
             case (timeSinceStart < animationDelay && i != 0):
-                sumPosition = canvas.transVList[i].slice(0, 2).reduce((a, b) => Math.abs(a) + Math.abs(b));
+                // Then compute the sum of the absolute values for it's x and y positions, so as to know if it's reached the center and can be deleted or no.
+                // Otherwise move it towards the center by dividing it's x and y coordinates by an ever growing value.
                 canvas.transVList[i] = [canvas.transVList[i][0]/(1 + (timeSinceStart+3) * 0.01), canvas.transVList[i][1]/(1 + (timeSinceStart+3) * 0.01), canvas.transVList[i][2]];
                 break;
+            // If this is the last run before the end of the intro:
             case (canvas.animationEnd):
+                // Increase the number of object to populate the soon-to-be-still background.
                 canvas.objectCount = 80;
+                // And set all current objects' life spans to 0 so as to get rid of them.
                 canvas.objectLifeSpan[i] = 0;
                 break
+            // If this is just a run of the still background animations:
             default:
+                // This code is used for smooth appearing and disapearing of objects in the background, use it when lowering the life span as mentionned above.
+                // // Init a value that defines how much does the object still has to live.
                 // let timeTillDeath = canvas.objectLifeSpan[i] - canvas.objectTimeAlive[i]
+                // // And depending on those cases:
                 // switch(true){
+                //     // If the object's in it's 2 first seconds of life:
                 //     case (canvas.objectTimeAlive[i] < 2):
+                //         // Then during this time, make it appear from the center, in other words (-100) from the back given perspective and toward it's intended depth.
                 //         canvas.transVList[i] = [canvas.transVList[i][0], canvas.transVList[i][1], Math.min((-100 * ((2-canvas.objectTimeAlive[i])/2)), canvas.depthList[i])];
                 //         break;
+                //     // If the object's in it's last 2 seconds of life:
                 //     case (timeTillDeath < 2):
+                //         // Do the opposite by decreasing it's depth value toward -100 so as to smoothly make it disapear.
                 //         canvas.transVList[i] = [canvas.transVList[i][0], canvas.transVList[i][1], Math.min((-100 * ((2-timeTillDeath)/2)), canvas.depthList[i])];
                 //         break;
                 //     default: 
+                //         // Otherwise do nothing.
                 //         break;
                 // }
-                sumPosition = canvas.transVList[i].reduce((a, b) => Math.abs(a) + Math.abs(b));
                 break;
         }
 
+        // Render the object by passing it's index to the main rendering method inside the canvas object.
         canvas.objectWiseRendering(i, deltaTime);
 
-        if ((sumPosition < 1 && timeSinceStart < animationDelay) || canvas.objectTimeAlive[i] > canvas.objectLifeSpan[i]){
+        // If the object's sum of positions is lower than 1 or if it's programmed end of life is reached: 
+        if (sumPosition < 1 || canvas.objectTimeAlive[i] > canvas.objectLifeSpan[i]){
+            // Remove it's entries from every array in the canvas, deleting it.
             canvas.transVList.splice(i, 1);
             canvas.objectLifeSpan.splice(i, 1);
             canvas.rotVList.splice(i,1);
             canvas.objectTimeAlive.splice(i, 1);
         }
     }
+    // No matter what, the animationEnd is always either not reached or already ran at this point in the code, hence we set it back to false.
     canvas.animationEnd = false;
 }
-// ------------------------------------------------------------------------------------------------------------------
--- Assets/app.js
+++ Assets/app.js
@@ -19,63 +19,61 @@ main();
 // We now define the main function which is called when the script is read.
 function main(){
     // ----------------------------------- Canvas Class Inits -----------------------------------
+    // For each canvas, we create an isntance of our custom canvas object using the desired html canvas ID.
     const canvasBackground = new Canvas('#Background_Spawner')
+    // We then set it up by giving it an RGBAV array that defines the clearing colour, and we also give it the path to the .obj file to render.
     canvasBackground.setup([235/255, 236/255, 237/255, 1, 1], 'Assets/Buffer_DATA/cube.obj')
-    canvasBackground.transVList.push([0.0, 0.0, -20.0]);
-    canvasBackground.rotVList.push([0.2, 0.4, 0.3]);
-    canvasBackground.objectTimeAlive.push(0.0);
-    canvasBackground.objectLifeSpan.push(animationDelay);
+    // We push all relevent data (translation vector, [initial rotation vector and continuous rotation vector], as well as optionnally the object's lifespan).
+    canvasBackground.pushObjectData([0.0, 0.0, -20.0], [0, 0, 0.3], animationDelay);
+    // And we add the canvas to the list of currently running ones.
     canvasesList.push(canvasBackground);
 
+    // We do the same for each canvas.
     const canvasMainBackground = new Canvas('#MainBackground_Canvas')
-    canvasMainBackground.setup([1, 1, 1, 0, 0], 'Assets/Buffer_DATA/gear2simple.obj', undefined, undefined, [1, 1, 1, 1]);
-    canvasMainBackground.transVList.push([0.0, 0.0, -10.0]);
-    canvasMainBackground.rotVList.push([0.2, 0.4, 0.3]);
-    canvasMainBackground.objectTimeAlive.push(0.0);
+    // This last parameter is the colour of the object, all the other one are transparent by default so as to see the image of video behind them.
+    canvasMainBackground.setup([1, 1, 1, 0, 0], 'Assets/Buffer_DATA/gear2simpleRot.obj', undefined, undefined, [1, 1, 1, 1]);
+    canvasMainBackground.pushObjectData([0.0, 0.0, -10.0], [0, 0, 0.05]);
+    // We want the background cog to spin more slowly so we change the rot speed from 10 to 20 (recall it's a dividend).
+    canvasMainBackground.rotationSpeed = 20;
+    canvasesList.push(canvasMainBackground);
 
     const canvasPCO = new Canvas('#PCO_Cube')
+    // The small poject cubes are of size 131px by 131px.
     canvasPCO.setup([1, 1, 1, 1, 0], 'Assets/Buffer_DATA/cube.obj', 131, 131);
-    canvasPCO.transVList.push([0.0, 0.0, -4.5]);
-    canvasPCO.rotVList.push([0.2, 0.4, 0.3]);
-    canvasPCO.objectTimeAlive.push(0.0);
+    canvasPCO.pushObjectData([0.0, 0.0, -4.5], [0.2, 0.4, 0.3]);
     canvasesList.push(canvasPCO);
 
     const canvasHunter = new Canvas('#Hunter_Cube')
     canvasHunter.setup([1, 1, 1, 1, 0], 'Assets/Buffer_DATA/cube.obj', 131, 131);
-    canvasHunter.transVList.push([0.0, 0.0, -4.5]);
-    canvasHunter.rotVList.push([-0.4, 0.2, 0.2]);
-    canvasHunter.objectTimeAlive.push(0.0);
+    canvasHunter.pushObjectData([0.0, 0.0, -4.5], [-0.4, 0.2, 0.2]);
     canvasesList.push(canvasHunter);
 
     const canvasAoC = new Canvas('#AdventOfCode_Cube')
     canvasAoC.setup([1, 1, 1, 1, 0], 'Assets/Buffer_DATA/cube.obj', 131, 131);
-    canvasAoC.transVList.push([0.0, 0.0, -4.5]);
-    canvasAoC.rotVList.push([0.1, 0.1, -0.4]);
-    canvasAoC.objectTimeAlive.push(0.0);
+    canvasAoC.pushObjectData([0.0, 0.0, -4.5], [0.1, 0.1, -0.4]);
     canvasesList.push(canvasAoC);
 
     const canvasIforme = new Canvas('#Iforme_Cube')
     canvasIforme.setup([1, 1, 1, 1, 0], 'Assets/Buffer_DATA/cube.obj', 131, 131);
-    canvasIforme.transVList.push([0.0, 0.0, -4.5]);
-    canvasIforme.rotVList.push([-0.2, 0.4, -0.2]);
-    canvasIforme.objectTimeAlive.push(0.0);
+    canvasIforme.pushObjectData([0.0, 0.0, -4.5], [-0.2, 0.4, -0.2]);
     canvasesList.push(canvasIforme);
 
     const canvasPages = new Canvas('#Pages_Cube')
     canvasPages.setup([1, 1, 1, 1, 0], 'Assets/Buffer_DATA/cube.obj', 131, 131);
-    canvasPages.transVList.push([0.0, 0.0, -4.5]);
-    canvasPages.rotVList.push([-0.2, -0.4, 0.4]);
-    canvasPages.objectTimeAlive.push(0.0);
+    canvasPages.pushObjectData([0.0, 0.0, -4.5], [-0.2, -0.4, 0.4]);
     canvasesList.push(canvasPages);
     // ------------------------------------------------------------------------------------------
     
     // ---------------------------------------- -Options ----------------------------------------
+    // Get the skip intro switch element.
     const introSkipSwitch = document.getElementById('IntroSkipValue');
     try{
+        // If there is a value for it already registered in the browser cache, then fetch it and read it.
         let introSkipStoredValue = JSON.parse(localStorage.getItem("introSkipStoredValue"));
+        // Then modify the switch's value to be the same as how the user last set it.
         introSkipSwitch.checked = introSkipStoredValue;
     } catch(error){
-        // Do nothing, it just means the user is going on this page for the first time.
+        // If no value is found, do nothing, it just means the user is going on this page for the first time.
     }
     // ------------------------------------------------------------------------------------------
 
@@ -83,18 +81,24 @@ function main(){
     // Set a timeout so as to start at the top of the page, we should clear the timeout when the animation skip button is clicked so as not to scroll back long after page load.
     const scrollTimeout = setTimeout(function(){window.scrollTo(0, 0)}, animationDelay*1000);
 
+    // Fetch the list of all elements with the .animated class.
     let animatedElements = document.querySelectorAll('.animated');
+    // Fetch the text in skills that reads "Technologies", since it is a bit long, we'll change it later to fit phones screen sizes.
     let rewriteTechH = document.getElementById('rewriteTech');
 
+    // Add an event listenener to the skipIntro function on click of the skip intro switch.
     introSkipSwitch.addEventListener('change', skipIntro);
 
+    // Add an event listener to resize the window's content each time the user resizes the window itself.
     window.addEventListener('resize', onResize);
 
+    // Add an event listenener for each of the project cube, they will redirect to the project they each talk about on click.
     canvasPCO.HTMLCanvas.addEventListener('click', function(){window.open("Assets/img/PCO_Paper.pdf")})
     canvasHunter.HTMLCanvas.addEventListener('click', function(){window.open("https://github.com/Teykowo/Reinforcement-Learning-Hunter")})
     canvasAoC.HTMLCanvas.addEventListener('click', function(){window.open("https://github.com/Teykowo/AdventOfCode-One_Language_A_Day")})
     canvasPages.HTMLCanvas.addEventListener('click', function(){window.open("https://github.com/Teykowo/Teykowo.github.io")})
     
+    // Create a function that iterates over all the animated element to make all their animation play right now and instantly.
     function terminateAnimations() {
         animatedElements.forEach((element) => {
             element.style.animationDelay = '0s, 0s'
@@ -102,33 +106,45 @@ function main(){
         });
     }
 
+    // Define a function that skips the intro animations it triggers when the "skip animations?" switch's state is changed.
     function skipIntro() {
+        // If the switch is checked:
         if (introSkipSwitch.checked){
+            // then clear the scroll up timeout as explained in it's setup.
             clearTimeout(scrollTimeout);
+            // Store the switch's value in the browser to fetch it on reload.
             localStorage.setItem("introSkipStoredValue", introSkipSwitch.checked);
+            // If the intro animation is still going:
             if (animationDelay > oldTime){
+                // Set it to end next frame.
                 animationDelay = oldTime;
-                canvasesList.forEach(canvas_i => {
-                    canvas_i.animationEnd = true;
-                });
+                canvasBackground.animationEnd = true;
 
+                // And terminate all the html elements' animations.
                 terminateAnimations();
             }
         }else{
+            // If the switch is unchecked then likewise store this value in the browser.
             localStorage.setItem("introSkipStoredValue", introSkipSwitch.checked);
         }
     }
 
+    // Define the onResize function which resizes the canvas so that their resolution fits the display.
     function onResize(){
+        // Run the resizeCanvas with the default value of "the whole screen" for both background canvases.
         canvasBackground.resizeCanvas();
         canvasMainBackground.resizeCanvas();
-        canvasesList.slice(1).forEach(canvas_i => {
+        // And resize the canvas to the decided size of 131px² for the projects cubes canvases (hence why the slice to ignore the first two (background) canvases).
+        canvasesList.slice(2).forEach(canvas_i => {
             canvas_i.resizeCanvas(131, 131);
         });
 
+        // If the window is less than 530 pixels wide:
         if (window.innerWidth < 530){
+            // Then rename the skill from "technologies" to "techs".
             rewriteTechH.textContent = 'Techs:';
         } else {
+            // Otherwise set it to back to "Technologies", so as not to keep the "Techs" if the screen goes back to a larger size.
             rewriteTechH.textContent = 'Technologies:';
         };
     }
@@ -137,7 +153,9 @@ function main(){
     skipIntro();
     onResize();
 
+    // Define the last animation to be played as the last CMD line entry appearance, and store it's animation delay to know when everything should be loaded.
     let lastAnimationTime = parseFloat(getComputedStyle(document.querySelector('#Final_REPL')).animationDelay.slice(0, 2));
+    // So as to avoid animations re-playing on screen size change (when their display goes from none to block), set all the animated elements animations to 0 delay 0 duration.
     setTimeout(terminateAnimations, lastAnimationTime*1000);
     // ------------------------------------------------------------------------------------------
 
@@ -159,9 +177,8 @@ function main(){
         // And set the old time variable to current time.
         oldTime = timeSinceStart;
 
-        // Call the draw function.
+        // Call the draw function. There is a specific one for the background hance the slice.
         drawBackground(canvasBackground, deltaTime, timeSinceStart);
-        canvasMainBackground.objectWiseRendering(0, deltaTime)
         canvasesList.slice(1).forEach(canvas_i => {
             canvas_i.objectWiseRendering(0, deltaTime);
         });
